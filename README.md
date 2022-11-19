81ae47119324b3e3196ed1eb2238882fdbdbb2f0
syildizz <118059676+syildizz@users.noreply.github.com>
2022-11-20 00:34:32 +0300

    Inital commit v2

---
+++ README.md
@@ -0,0 +1,71 @@
+# IEEErepr
+
+IEEErepr is small Python library which gives you the IEEE-754 representation of a floating point number. IEEErepr was developed by [Saygın Efe Yıldız](https://github.com/syildizz) and was made with inspiration from the [ieee754](https://github.com/canbula/ieee754) project made by [Bora Canbula](https://github.com/canbula)
+
+When creating the representation you can specify a precision from the list given below or you can even use your own custom precision.
+<ul>
+<li>Half Precision (16 bit: 1 bit for sign + 5 bits for exponent + 10 bits for mantissa)</li>
+<li>Single Precision (32 bit: 1 bit for sign + 8 bits for exponent + 23 bits for mantissa)</li>
+<li>Double Precision (64 bit: 1 bit for sign + 11 bits for exponent + 52 bits for mantissa)</li>
+<li>Quadruple Precision (128 bit: 1 bit for sign + 15 bits for exponent + 112 bits for mantissa)</li>
+<li>Octuple Precision (256 bit: 1 bit for sign + 19 bits for exponent + 236 bits for mantissa)</li>
+</ul>
+
+## Installing
+
+To install IEEErepr, you have to clone this github repo and put the ieee.py file in the appropriate location.
+
+## Using
+After installation, you can import ieee754 and use it in your projects.
+
+### Select a Precision
+You can use half, double, quadruple, octuple or custom precision by specifying them in the arguments as thus:
+```Python
+from ieee import IEEErepr
+
+#for half precision
+a = IEEErepr("5.0123", "HALF")
+#for single precision
+b = IEEErepr(0.0013, "FLOAT")
+#for quadruple precision
+c = IEEErepr(256, "QUADRUPLE")
+#for octuple precision
+d = IEEErepr("234_546", "OCTUPLE")
+print(a)
+print(a.get_mantissa())
+print(b.get_binary())
+print(c.get_hex())
+print(d.get_exponent())
+```
+
+### Using a Custom Precision
+You can define the size of the exponent and the mantissa in the third and fourth arguments respectively.
+```Python
+#for custom precision
+e = ieee.IEEErepr("0.0", "CUSTOM", 5, 10)
+print(e.get_sign)
+#where first integer argument is exponent size,
+#second integer argument is mantissa size
+```
+Note that if the precision argument isn't "CUSTOM", the size values specified get ignored when generating the representation
+
+## Known Issues
+<ul>
+<li>No rounding up is done on the numbers given the precision so the last bit is likely wrong</li>
+</ul>
+
+
+
+
+License
+----
+
+MIT License
+
+Copyright (c) 2022 Saygın Efe Yıldız
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ ieee.py
@@ -0,0 +1,144 @@
+
+class IEEErepr:
+
+    #__length_list = [16, 32, 64, 128, 256]
+    __exponent_list = [5, 8, 11, 15, 19]
+    __mantissa_list = [10, 23, 52, 112, 236]
+    __bias_list = [15, 127, 1023, 16_383, 262_143]
+
+    HALF = 0
+    FLOAT = 1
+    DOUBLE = 2
+    QUADRUPLE = 3
+    OCTUPLE = 4
+    CUSTOM = -1
+    POSITIVE = '0'
+    NEGATIVE = '1'
+
+    class UndefinedArgumentException(Exception):
+        pass
+
+    class NotAValidNumberException(Exception):
+        pass
+
+    def __init__ (self, number, prec: str, exp_num = None, mantissa_num = None):
+
+        try:
+            self.__prec = self.__is_valid_prec(prec, exp_num, mantissa_num)
+        
+        except IEEErepr.UndefinedArgumentException:
+            print("UndefinedArgumentException: Not a valid precision argument")
+            print("Valid precision arguments:")
+            print("HALF: 16 bit, FLOAT: 32 bit, DOUBLE: 64 bit, QUADRUPLE: 128 bit, OCTUPLE: 256 bit, CUSTOM: custom")
+            print("Note: CUSTOM must include exponent and mantissa number value in that order")
+            exit()
+        
+        #self.__length_val = IEEErepr.__length_list[self.__prec]
+        try:
+            self.__number = self.__is_valid_num(number)
+
+        except IEEErepr.NotAValidNumberException:
+            print("NotAValidNumberException: The inputted value was not a valid number")
+            print("Valid numbers include:")
+            print("\"XX\", \"XX.XX\", XX, XX.XX")
+            exit()
+
+        self.__exponent_val = IEEErepr.__exponent_list[self.__prec] if self.__prec != IEEErepr.CUSTOM else int(exp_num)  # type: ignore
+        self.__mantissa_val = IEEErepr.__mantissa_list[self.__prec] if self.__prec != IEEErepr.CUSTOM else int(mantissa_num)  # type: ignore
+        self.__bias_val = IEEErepr.__bias_list[self.__prec] if self.__prec != IEEErepr.CUSTOM else (2 ** (self.__exponent_val - 1)) - 1
+        self.__sign = IEEErepr.NEGATIVE if self.__number[0] == '-' else IEEErepr.POSITIVE
+        self.__split_num = self.__num_splitter()
+        self.__bin_num = self.__bin_2_int() + self.__bin_2_dec()
+        if self.__bin_num.count('1') != 0:
+            self.__mantissa = self.__bin_num[self.__bin_num.index('1') + 1:self.__bin_num.index('1') + self.__mantissa_val + 1]
+            self.__exponent = self.__exponent_2_bin()
+        else:
+            self.__mantissa = '0' * (self.__mantissa_val)
+            self.__exponent = '0' * (self.__exponent_val)
+        self.__float = self.__sign + self.__exponent + self.__mantissa
+
+    def __str__(self):
+        return self.__float
+
+    def get_binary(self):
+        return self.__float
+
+    # Following piece of code taken from: https://stackoverflow.com/a/2072384
+    def get_hex(self):
+        return '%0*X' % ((len(self.__float) + 3) // 4, int(self.__float, 2))
+
+    def get_mantissa(self):
+        return self.__mantissa
+
+    def get_sign(self):
+        return self.__sign
+
+    def get_exponent(self):
+        return self.__exponent
+
+    def __is_valid_num(self, number):
+        number = str(number).replace(',', '').replace('_', '').replace(' ', '')
+        if '.' not in number:
+            number += ".0"
+        if not number.replace('.', '').replace('-', '').isdigit() or number.count('.') != 1:
+                raise IEEErepr.NotAValidNumberException
+        return number
+
+    def __is_valid_prec(self, prec, e_num, m_num):
+        prec = str(prec)
+        prec.upper()
+        if prec == "HALF":
+            return IEEErepr.HALF
+        elif prec == "FLOAT":
+            return IEEErepr.FLOAT
+        elif prec == "DOUBLE":
+            return IEEErepr.DOUBLE
+        elif prec == "QUADRUPLE":
+            return IEEErepr.QUADRUPLE
+        elif prec == "OCTUPLE":
+            return IEEErepr.OCTUPLE
+        elif prec == "CUSTOM" and str(e_num).isdigit() and str(m_num).isdigit():
+            return IEEErepr.CUSTOM
+        else:
+            raise IEEErepr.UndefinedArgumentException
+        
+    def __bin_2_dec(self):
+        def __bin_2_dec_itr():
+            nonlocal tempstr
+            nonlocal temp
+            nonlocal temp2
+            temp *= 2
+            if temp >= temp2:
+                tempstr += '1'
+                temp -= temp2
+            else:
+                tempstr += '0'
+            return tempstr
+        tempstr = ""
+        temp = int(self.__split_num[1])
+        if temp != 0:
+            temp2 = 10**(len(self.__split_num[1]))
+        else:
+            return '0' * (self.__mantissa_val) #replaced + 1 with * 2 
+        i = 0
+        while (tempstr.count('1') == 0):
+            __bin_2_dec_itr()
+            i += 1
+        for _ in range(0,self.__mantissa_val + i):
+            __bin_2_dec_itr()
+        return tempstr
+    
+    def __bin_2_int(self):
+        return bin(int(self.__split_num[0]))[2:]
+
+    def __exponent_2_bin(self):
+        float_amount = len(self.__bin_2_int()) - (self.__bin_num.index('1') + 1) 
+        float_amount_bin = bin(self.__bias_val + float_amount)[2:self.__exponent_val + 2]
+        while len(float_amount_bin) < self.__exponent_val:
+            float_amount_bin = '0' + float_amount_bin
+        return float_amount_bin
+    
+    def __num_splitter(self):
+        self.__abs_num = self.__number if self.__sign is IEEErepr.POSITIVE else self.__number[1:]
+        return self.__abs_num.split('.')
+    
